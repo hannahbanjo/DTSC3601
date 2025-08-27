00a78f382d796d6db8a59c800388ca32b296fa82
hannahbanjo <wilson.hannah.mel@gmail.com>
2025-08-27 15:12:39 -0400

    script to print supabase table

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-import this
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+# Load .env file
+load_dotenv()
+
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(url, key)
+
+# Example select
+response = supabase.table("quilts").select("*").execute()
+print(response.data)
